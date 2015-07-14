e82a1a45fb32dce1d0d09f9a1e3f032f42c73297
Lisa Davis <oobbles@gmail.com>
2015-07-13 21:24:31 -0500

    Have 3 controllers working- all assignments, one assignment and all the links for one given assignment

--- controllers/api_controller.rb
+++ controllers/api_controller.rb
@@ -7,35 +7,22 @@ get "/api/assignments" do
 #   Assignment.all.each do |object|
 #     @everything<<object.to_hash
  end
+
+ #this gives you one assignment based on id
  get "/api/assignments/:id" do
    id = params["id"].to_i
    @assignment=Assignment.find(id).to_hash
    json(@assignment)
  end
-#NONE OF THIS WORKS
-# get "/api/assignments/:id" do
-#   id = params["id"].to_i
-#   assignment=Assignment.find(id)
-#   link_array=assignment.find_links
-#   json_array=assignment.to_hash
-#   link_names=[]
-#   link_array.each do |link_id|
-#     link_names<<fin
-#   links=Assignment.find(id).find_links
-#   @links=[]
-#   Assignment
-#   # collabs=Assignment.find(id).find_collabs.to_hash
-#   json(@one_assignment)
-# return links.to_hash
-#  end
-#  get "/api/assignments/:id" do
-#    assignment = Assignment.find(params["id"])
-#    collaborator_array = assignment.find_collaborators
-#    json_array = assignment.json_format
-#    collaborator_names = []
-#    collaborator_array.each do |collaborator_id|
-#      collaborator_names << Collaborator.find(collaborator_id).collaborator_name
-#    end
-#    json_array["collaborators"] = collaborator_names
-#    json json_array
-#  end
+
+#This gives you the links associated with a specific assignment
+get "/api/assignment_links/:id" do
+  id = params["id"].to_i
+  assignment=Assignment.find(id) #an assignment object?
+  link_array=assignment.find_links
+  link_names=[]
+  link_array.each do |link_object|
+    link_names<<link_object.link
+end
+json link_names
+end
